de68cf229016659e7fad2f50e4e5a882102be53b
Igor Antonov <homyaga@gmail.com>
2025-10-22 13:09:48 +0300

    fix: добавлено раздельное подключение tailwind на проде и на дев

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./templates/**/*.html",
-    "./*/templates/**/*.html",
-    "./static/js/**/*.js",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'primary': '#2c5aa0',
-        'secondary': '#f8f9fa',
-        'accent': '#ffc107',
-      },
+    content: [
+        "./templates/**/*.html",
+        "./*/templates/**/*.html",
+        "./apps/**/templates/**/*.html",
+        "./static/js/**/*.js",
+        "./staticfiles/js/**/*.js",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#474B57",
+                primaryDark: "#333847",
+                secondary: "#f8f9fa",
+                accent: "#F1B400",
+                tertiary: "#616677",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
